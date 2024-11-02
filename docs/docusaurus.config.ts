05f5ad5ca02364b3341023e82d68f4303aa3c9e1
Joel Campos <joelcamposoliva9000@gmail.com>
2024-11-02 10:42:34 +0100

    docs: add edit url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -37,8 +37,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/TheMineWay/statenet/tree/main/docs",
         },
         theme: {
           customCss: "./src/css/custom.css",
