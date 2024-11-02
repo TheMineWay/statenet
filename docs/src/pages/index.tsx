d1bb96eb041c503a63e5411d1b40e410801da6ea
Joel Campos <joelcamposoliva9000@gmail.com>
2024-11-02 11:41:14 +0100

    docs: branding

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,45 +1,61 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import Heading from "@theme/Heading";
-
+import Tgle from "@theme/ColorModeToggle";
 import styles from "./index.module.css";
-import MainLanding from "../components/home/main-landing";
+import { ColorMode, useColorMode } from "@docusaurus/theme-common";
+import { ColorModeProvider } from "@docusaurus/theme-common/internal";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import clsx from "clsx";
+import pk from "../../package.json";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+export default function Index() {
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
+    <ColorModeProvider>
+      <Content />
+    </ColorModeProvider>
+  );
+}
+
+const Content = () => {
+  const mode = useColorMode();
+  const config = useDocusaurusContext();
+  return (
+    <div className={styles.content}>
+      <div />
+      <div>
+        <h1 className={styles.title}>
+          State<span className={styles.net}>.NET</span>
+        </h1>
+        <p>{config.siteConfig.tagline}</p>
+        <div className={clsx(styles.centered, styles.actions)}>
+          <a
+            className="button button--outline button--secondary"
+            href="docs/intro"
           >
-            Get started 🔥
-          </Link>
+            Docs
+          </a>
+          <a
+            className="button button--outline button--secondary"
+            href={pk.repository.url}
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            className="button button--outline button--secondary"
+            href="https://www.nuget.org/packages/TheMineWay.StateNet"
+            target="_blank"
+          >
+            NuGet
+          </a>
         </div>
       </div>
-    </header>
+      <div className={styles.centered}>
+        <Tgle
+          value={mode.colorMode}
+          onChange={function (colorMode: ColorMode): void {
+            mode.setColorMode(colorMode);
+          }}
+        />
+      </div>
+    </div>
   );
-}
-
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <MainLanding />
-      </main>
-    </Layout>
-  );
-}
+};
