b839b8c07416083363647df297af79c9f9634bf9
Joel Campos <joelcamposoliva9000@gmail.com>
2024-11-24 11:16:32 +0100

    feat: rename T to A

--- StateNet/State.cs
+++ StateNet/State.cs
@@ -2,12 +2,12 @@
 
 namespace StateNet
 {
-    public class State<S, T, C> where S : IComparable where T : IComparable
+    public class State<S, A, C> where S : IComparable where A : IComparable
     {
-        public delegate void OnStateEvent(TransitionInfo<S,T,C> transitionInfo);
+        public delegate void OnStateEvent(TransitionInfo<S,A,C> transitionInfo);
 
-        public readonly Dictionary<T, List<Transition<S, T, C>>> transitions = [];
-        internal State(Dictionary<T, List<Transition<S, T, C>>>? transitions = null, OnStateEvent? onEnter = null, OnStateEvent? onExit = null) {
+        public readonly Dictionary<A, List<Transition<S, A, C>>> transitions = [];
+        internal State(Dictionary<A, List<Transition<S, A, C>>>? transitions = null, OnStateEvent? onEnter = null, OnStateEvent? onExit = null) {
             if (transitions != null) this.transitions = transitions;
 
             // Register default events
@@ -18,38 +18,38 @@ namespace StateNet
         #region Events
 
         private OnStateEvent? onEnter;
-        internal void InvokeOnEnter(TransitionInfo<S, T, C> transitionInfo) => onEnter?.Invoke(transitionInfo);
+        internal void InvokeOnEnter(TransitionInfo<S, A, C> transitionInfo) => onEnter?.Invoke(transitionInfo);
 
         private OnStateEvent? onExit;
-        internal void InvokeOnExit(TransitionInfo<S, T, C> transitionInfo) => onExit?.Invoke(transitionInfo);
+        internal void InvokeOnExit(TransitionInfo<S, A, C> transitionInfo) => onExit?.Invoke(transitionInfo);
 
         #endregion
 
         #region API
 
-        public Transition<S, T, C> AddTransition(T action, S targetStateName) => AddTransition(action, new Transition<S, T, C>(targetStateName));
-        public Transition<S, T, C> AddTransition(T action, Transition<S, T, C> transition)
+        public Transition<S, A, C> AddTransition(A action, S targetStateName) => AddTransition(action, new Transition<S, A, C>(targetStateName));
+        public Transition<S, A, C> AddTransition(A action, Transition<S, A, C> transition)
         {
             if (!transitions.ContainsKey(action)) transitions[action] = [];
             transitions[action].Add(transition);
             return transition;
         }
 
-        public State<S, T, C> RemoveTransitions(T action)
+        public State<S, A, C> RemoveTransitions(A action)
         {
             transitions.Remove(action);
             return this;
         }
 
-        internal List<Transition<S, T, C>> GetSortedActionTransitionsList(T action) => [.. transitions[action].OrderByDescending(item => item.IsConditional())];
+        internal List<Transition<S, A, C>> GetSortedActionTransitionsList(A action) => [.. transitions[action].OrderByDescending(item => item.IsConditional())];
 
-        internal Transition<S, T, C>? GetTransitionByAction(StateMachine<S, T, C> machine, T action)
+        internal Transition<S, A, C>? GetTransitionByAction(StateMachine<S, A, C> machine, A action)
         {
             foreach (var transition in GetSortedActionTransitionsList(action))
             {
                 if (!transition.IsConditional()) return transition;
 
-                var transitionInfo = new TransitionInfo<S, T, C>
+                var transitionInfo = new TransitionInfo<S, A, C>
                 {
                     Via = action,
                     Machine = machine,
@@ -64,13 +64,13 @@ namespace StateNet
 
         // State API
 
-        public State<S, T, C> OnEnter(OnStateEvent onEnter)
+        public State<S, A, C> OnEnter(OnStateEvent onEnter)
         {
             this.onEnter += onEnter;
             return this;
         }
 
-        public State<S, T, C> OnExit(OnStateEvent onExit)
+        public State<S, A, C> OnExit(OnStateEvent onExit)
         {
             this.onExit += onExit;
             return this;
--- StateNet/StateMachine.cs
+++ StateNet/StateMachine.cs
@@ -2,7 +2,7 @@
 
 namespace StateNet
 {
-    public class StateMachine<S, T, C> where S : IComparable where T : IComparable
+    public class StateMachine<S, A, C> where S : IComparable where A : IComparable
     {
         #region Factory
         internal StateMachine(S initialState, C? initialContext) {
@@ -10,9 +10,9 @@ namespace StateNet
             Context = initialContext;
         }
 
-        public static Func<StateMachine<S, T, C>> Factory(Action<MutableStateMachine<S, T, C>> builder, S initialState, C? initialContext = default) => () =>
+        public static Func<StateMachine<S, A, C>> Factory(Action<MutableStateMachine<S, A, C>> builder, S initialState, C? initialContext = default) => () =>
         {
-            var machine = new MutableStateMachine<S, T, C>(initialState, initialContext);
+            var machine = new MutableStateMachine<S, A, C>(initialState, initialContext);
             builder(machine);
             return machine;
         };
@@ -21,7 +21,7 @@ namespace StateNet
 
         #region API
 
-        public void Trigger(T action)
+        public void Trigger(A action)
         {
             // Check if machine reached a end state
             if (!states.ContainsKey(CurrentState)) return;
@@ -38,7 +38,7 @@ namespace StateNet
             CurrentState = transition.targetState; // Change current state
 
             // Get transition info
-            TransitionInfo<S, T, C> transitionInfo = new() { FromState = oldStateName, ToState = transition.targetState, Via = action, Machine = this };
+            TransitionInfo<S, A, C> transitionInfo = new() { FromState = oldStateName, ToState = transition.targetState, Via = action, Machine = this };
 
             // Trigger transition with context info (action and machine)
             transition.Transitate(transitionInfo);
@@ -72,7 +72,7 @@ namespace StateNet
         #region Info
 
         public S CurrentState { get; protected set; }
-        readonly protected Dictionary<S, State<S, T, C>> states = [];
+        readonly protected Dictionary<S, State<S, A, C>> states = [];
 
         public C? Context { get; protected set; }
 
